5325314b65baa4c848e339f12a248d6b27152d0c
ilhambara <g2a.ilhammubarok@gmail.com>
2022-09-17 09:04:41 +0700

    update: promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,13 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (hasil) => {
+  const theaterIXX = await promiseTheaterIXX();
+  const theaterVGC = await promiseTheaterVGC();
+  const arrTheater = [...theaterIXX, theaterVGC];
+
+  return arrTheater.filter((item) => item.hasil === hasil).length;
+};
 
 module.exports = {
   promiseOutput,
